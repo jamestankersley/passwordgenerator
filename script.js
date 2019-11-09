4cf96bce44dfa85824adf33a5f9a14e05df9ef82
James <jwtankersley10@gmail.com>
2019-11-09 10:35:00 -0600

    changed js to debug prompts to be responsive to data entry

--- script.js
+++ script.js
@@ -1,34 +1,92 @@
-function generate(){
-    //set password length/complexity
-    var complexity = document.getElementById("slider").value;
-    //possible password values
-    var values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
-    var password = "";
-    //create for loop to choose password characters
-    for(var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+ 
+ //when the Generate Password button is clicked, the following function should run//
+ function generatePassword() {
+
+    //create variables for each set of characters that can be pulled into the password//
+
+    var stringNumbers = "0123456789";
+
+    var stringUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var stringLowerCase = "abcdefghijklmnopqrstuvwxyz";
+
+    var stringSpecial = "!#$%&'()*+,-./:;<=>?@[]\^_`{|}~";
+
+    //Empty variable for characters to pull into based on user selections//
+    var newPassword = "";
+
+    //Empty variable to collect string arrays based on userCharacter choices//
+    var newString = "";
+
+    
+    //Prompt user to enter the number of characters//
+    var userCharLength = prompt("How many characters do you want your password to be? (between 8 - 128)");
+
+    //if there is no character entered, or length is less than 8 or more than 128, prompt user regarding parameters//
+
+    if (userCharLength === "" || userCharLength < 8 || userCharLength > 128) {
+      alert ("Character length must be between 8 and 128");
     }
-    //add password to textbox/display area
-    document.getElementById("display").value = password;
-    //add password to previously generated passwords section
-    document.getElementById("lastNums").innerHTML += password + "<br>";
-   }
-   //set default length display of 25
-   document.getElementById("length").innerHTML = "Length: 25";
-   //function to set length based on slider position
-   document.getElementById("slider").oninput = function(){
-    if(document.getElementById("slider").value > 8){
-        document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+
+    //with above condition met, continue to ask user on type of characters to be included in password//
+    else {
+
+    var userChoiceNumbers = confirm("Add numeric characters?  (ex/0123)");
+
+    var userChoiceUpperCase = confirm("Add uppercase characters?  (ex/ABC)");
+
+    var userChoiceLowerCase = confirm("Add lowercase characters?  (ex/abc)");
+    
+    var userChoiceSpecial = confirm("Add special characters?  (ex/?!K@)");
+
+    //fill newString variable with Strings based on user selection//
+
+      if (userChoiceSpecial) {
+        newString += stringSpecial;
+      }
+      
+      if (userChoiceNumbers) {
+        newString += stringNumbers;
+      }
+        
+      if (userChoiceUpperCase) {
+        newString += stringUpperCase;
+      }
+          
+      if (userChoiceLowerCase) {
+        newString += stringLowerCase;
+      }
+
+      if (newString === "") {
+        alert("NOTE: You must choose a character type to display a password.");
+        
+      }
+    
     }
-    else{
-        document.getElementById("length").innerHTML = "Length: 8";
+
+    //use a for loop to fill newPassword from newString until length of characters requested by user are met//
+    for (var i = 0; i < userCharLength; i++) {
+      
+    newPassword = newPassword + newString.charAt(Math.floor(Math.random() * Math.floor(newString.length))); 
     }
-   }
-   //function to copy password to clipboard
-   function copyPassword(){
-    document.getElementById("display").select();
-    document.execCommand("Copy");
-    alert("Password copied to clipboard!");
-   }
-   
+
+    //add newPassword to Generate Password box in displaypw text input//
+    document.getElementById("displaypw").value = newPassword;
+
+}
+
+//when the Copy to Clipboard button is clicked, the following function should run//
+function copyToClipboard() {
+    //grab displaypw input from which the password can be copied//
+    var copyText = document.getElementById("displaypw");
+  
+    //select and copy text//
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+
+    //replaces password with text indicating it has been copied to clipboard//
+    document.getElementById("displaypw").value = "Password copied to clipboard; use paste or ctrl v."
+  
+  }
 
